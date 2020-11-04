2aa579821f0fb739d4bdbfb8ef83609b951da2d2
LASTLINEK <2532322313@qq.com>
2020-11-04 12:00:41 +0800

    UI和部分数据结构

---
+++ RKDBMS/RKDBMSView.cpp
@@ -0,0 +1,128 @@
+﻿
+// RKDBMSView.cpp: CRKDBMSView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "RKDBMS.h"
+#endif
+
+#include "RKDBMSDoc.h"
+#include "RKDBMSView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CRKDBMSView
+
+IMPLEMENT_DYNCREATE(CRKDBMSView, CView)
+
+BEGIN_MESSAGE_MAP(CRKDBMSView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CRKDBMSView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CRKDBMSView 构造/析构
+
+CRKDBMSView::CRKDBMSView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CRKDBMSView::~CRKDBMSView()
+{
+}
+
+BOOL CRKDBMSView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CRKDBMSView 绘图
+
+void CRKDBMSView::OnDraw(CDC* /*pDC*/)
+{
+	CRKDBMSDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CRKDBMSView 打印
+
+
+void CRKDBMSView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CRKDBMSView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CRKDBMSView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CRKDBMSView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CRKDBMSView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CRKDBMSView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CRKDBMSView 诊断
+
+#ifdef _DEBUG
+void CRKDBMSView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CRKDBMSView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CRKDBMSDoc* CRKDBMSView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CRKDBMSDoc)));
+	return (CRKDBMSDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CRKDBMSView 消息处理程序
---
+++ RKDBMS/RKDBMSView.h
@@ -0,0 +1,52 @@
+﻿
+// RKDBMSView.h: CRKDBMSView 类的接口
+//
+
+#pragma once
+
+
+class CRKDBMSView : public CView
+{
+protected: // 仅从序列化创建
+	CRKDBMSView() noexcept;
+	DECLARE_DYNCREATE(CRKDBMSView)
+
+// 特性
+public:
+	CRKDBMSDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CRKDBMSView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // RKDBMSView.cpp 中的调试版本
+inline CRKDBMSDoc* CRKDBMSView::GetDocument() const
+   { return reinterpret_cast<CRKDBMSDoc*>(m_pDocument); }
+#endif
+
